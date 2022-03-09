3de9ba50332d39f5cff379de999cd0d2585149a7
school mac <chestercheetah@honkbot.de>
2022-03-09 12:30:30 -0600

    widets wow!

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 #   a214_simple_window1.py
 #   A program creates a window on your screen using Tkinter
 import tkinter as tk
-
 # main window
 root = tk.Tk()
 root.wm_geometry("200x100")
+root.title('authentrication')
+
+frame_login = tk.Frame(root)
+frame_login.grid()
+
+
+lbl_username = tk.Label(frame_login, text='userenanem:')
+lbl_username.pack()
+
 
 root.mainloop()
